4f373f0f3b90d37ad56a7cde10e524ec73ae75e6
Ezrovat <danic97@gmail.com>
2026-02-14 17:06:59 +0100

    static page init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    watchOptions: {
+        poll: 1000,
+        ignored: /node_modules/,
+    },
+    devServer: {
+        static: "./dist",
+        watchFiles: ["./src/**/*"],
+        hot: true,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
+
+        ]
+    }
+}
